9c5987e73c3092930d77a21a5f634f2a4157290f
Tien-Thinh Nguyen <Tien-Thinh.Nguyen@eurecom.fr>
2021-06-21 23:34:14 +0200

    code cleanup

--- build/ext/json/_build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maserati/oai-cn5g-udr/build/ext/json
-# Build directory: /home/maserati/oai-cn5g-udr/build/ext/json/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
