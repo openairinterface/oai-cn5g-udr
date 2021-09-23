bd3fe95d8728c80948db0a7fc1ebd72b0d35ddf3
Tien-Thinh Nguyen <Tien-Thinh.Nguyen@eurecom.fr>
2021-09-23 15:08:28 +0200

    Fix issue for SSC Mode

--- src/api_server/model/SscMode.cpp
+++ src/api_server/model/SscMode.cpp
@@ -43,6 +43,10 @@ void SscMode::validate() {
   // TODO: implement validation
 }
 
+void SscMode::set_value(std::string value) { this->value = value; }
+void SscMode::get_value(std::string &value) const { value = this->value; }
+std::string SscMode::get_value() const { return value; }
+
 void to_json(nlohmann::json &j, const SscMode &o) { j = o.get_value(); }
 
 void from_json(const nlohmann::json &j, SscMode &o) {
--- src/api_server/model/SscMode.h
+++ src/api_server/model/SscMode.h
@@ -52,6 +52,9 @@ class SscMode {
   virtual ~SscMode();
 
   void validate();
+  void set_value(std::string value);
+  void get_value(std::string &value) const;
+  std::string get_value() const;
 
   /////////////////////////////////////////////
   /// SscMode members
