4705244b71a80c472fb482830e0d6e323d58a5ba
Mohammed Ismail <mohammed.ismail@openairinterface.org>
2021-11-03 14:05:47 +0100

    entrypoint updated to handle already set config fi
    Signed-off-by: Mohammed Ismail <mohammed.ismail@openairinterface.org>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,6 +7,10 @@ UDR_INTERFACE_PORT_FOR_NUDR=${UDR_INTERFACE_PORT_FOR_NUDR:-80}
 
 for c in ${CONFIG_DIR}/*.conf; do
     # grep variable names (format: ${VAR}) from template to be rendered
+    if ! grep -oP '@[a-zA-Z0-9_]+@' ${c}; then
+        echo "Configuration is already set"
+        exec "$@"
+    fi
     VARS=$(grep -oP '@[a-zA-Z0-9_]+@' ${c} | sort | uniq | xargs)
 
     # create sed expressions for substituting each occurrence of ${VAR}
