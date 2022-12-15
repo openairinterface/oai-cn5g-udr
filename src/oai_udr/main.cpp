b5d8c59b50f873bd79a122b6c726e4a9a7fdf88d
Raphael Defosseux <raphael.defosseux@eurecom.fr>
2022-12-15 14:27:30 +0100

    fix(exit): catching SIGTERM
    Signed-off-by: Raphael Defosseux <raphael.defosseux@eurecom.fr>

--- src/oai_udr/main.cpp
+++ src/oai_udr/main.cpp
@@ -78,11 +78,8 @@ int main(int argc, char** argv) {
   Logger::init("udr", Options::getlogStdout(), Options::getlogRotFilelog());
   Logger::udr_server().startup("Options parsed");
 
-  struct sigaction sigIntHandler;
-  sigIntHandler.sa_handler = my_app_signal_handler;
-  sigemptyset(&sigIntHandler.sa_mask);
-  sigIntHandler.sa_flags = 0;
-  sigaction(SIGINT, &sigIntHandler, NULL);
+  std::signal(SIGTERM, my_app_signal_handler);
+  std::signal(SIGINT, my_app_signal_handler);
 
   // Event subsystem
   udr_event ev;
